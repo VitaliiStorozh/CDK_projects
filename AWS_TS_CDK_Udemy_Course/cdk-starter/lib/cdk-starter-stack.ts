6493f695f5497509c1f5543a70a31f6121a33e04
Vitalii Storozh <storozh.softserve@gmail.com>
2023-06-07 21:36:53 +0300

    Constructs

--- AWS_TS_CDK_Udemy_Course/cdk-starter/lib/cdk-starter-stack.ts
+++ AWS_TS_CDK_Udemy_Course/cdk-starter/lib/cdk-starter-stack.ts
@@ -1,8 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { Duration, Stack } from 'aws-cdk-lib';
 
-export class CdkStarterStack extends cdk.Stack {
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      // bucketName: 'my-temp-bucket',
+      lifecycleRules: [
+        {
+          expiration: Duration.days(expiration),
+        },
+      ],
+    });
+  }
+}
+
+export class CdkStarterStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create a S3 bucket in 3 ways:
+    new CfnBucket(this, 'MyL1Bucket', {
+      // bucketName: 'my-temp-bucket',
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: 'Enabled',
+          },
+        ],
+      },
+    });
+
+    new Bucket(this, 'MyL2Bucket', {
+      // bucketName: 'my-temp-bucket',
+      lifecycleRules: [
+        {
+          expiration: Duration.days(2),
+        },
+      ],
+    });
+
+    new L3Bucket(this, 'MyL3Bucket', 3);
   }
 }
