5591cbfb3f6db032bede3046c62dde8abc1d8c72
Vitalii Storozh <storozh.softserve@gmail.com>
2023-06-14 22:22:24 +0300

    CDK Aspects

--- AWS_TS_CDK_Udemy_Course/cdk-starter/bin/cdk-starter.ts
+++ AWS_TS_CDK_Udemy_Course/cdk-starter/bin/cdk-starter.ts
@@ -2,10 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotosStack } from '../lib/PhotosStack';
-import {PhotosHandlerStack} from "../lib/PhotosHandlerStack";
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTugger } from './tagger';
+import { HIPAASecurityChecks } from 'cdk-nag';
 
 const app = new cdk.App();
 const photoStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
-    targetBucketArn: photoStack.photosBucketArn,
+  targetBucketArn: photoStack.photosBucketArn,
 });
+
+const tagger = new BucketTugger('environment', 'dev');
+cdk.Aspects.of(app).add(tagger);
+cdk.Aspects.of(app).add(new HIPAASecurityChecks());
