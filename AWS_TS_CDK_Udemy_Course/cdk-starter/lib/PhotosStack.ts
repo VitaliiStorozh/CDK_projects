5b1d18959f61c945b0e6f2444a272d9209d5294a
Vitalii Storozh <storozh.softserve@gmail.com>
2023-06-14 20:26:15 +0300

    CDK IDs

--- AWS_TS_CDK_Udemy_Course/cdk-starter/lib/PhotosStack.ts
+++ AWS_TS_CDK_Udemy_Course/cdk-starter/lib/PhotosStack.ts
@@ -1,14 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
-import { Stack } from 'aws-cdk-lib';
+import { Fn, Stack } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotosStack extends Stack {
+  private stackSufix: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new Bucket(this, 'PhotosBucket', {
-      bucketName: 'photos-bucket-1234567890',
+    this.initializeStackSufix();
+
+    new Bucket(this, 'PhotosBucket', {
+      bucketName: `photos-bucket-${this.stackSufix}`,
     });
+  }
+
+  private initializeStackSufix() {
+    const shortStacId = Fn.select(2, Fn.split('/', this.stackId));
+    this.stackSufix = Fn.select(4, Fn.split('-', shortStacId));
   }
 }
