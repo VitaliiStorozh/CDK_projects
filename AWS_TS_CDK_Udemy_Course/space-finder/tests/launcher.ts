a4bd3ef0ea91f064456c7a499a943b5e69916d26
Vitalii Storozh <storozh.softserve@gmail.com>
2023-06-30 22:28:52 +0300

    DynamoDb GetItem operation

--- AWS_TS_CDK_Udemy_Course/space-finder/tests/launcher.ts
+++ AWS_TS_CDK_Udemy_Course/space-finder/tests/launcher.ts
@@ -6,6 +6,9 @@ process.env.TABLE_NAME = 'SpaceTable-0e727129acdb';
 handler(
   {
     httpMethod: 'GET',
+    queryStringParameters: {
+      id: 'ad0c8a11-cf98-4191-b843-baeb4b6ebbe6',
+    },
     // body: JSON.stringify({
     //   location: 'Dublin',
     // }),
