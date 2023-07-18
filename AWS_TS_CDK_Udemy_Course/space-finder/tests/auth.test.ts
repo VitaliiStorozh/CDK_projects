6f5442b1b015843c7f3a92498b46bfe42ab44655
Vitalii Storozh <storozh.softserve@gmail.com>
2023-07-18 08:54:28 +0300

    Testing AWS temporary credentials

--- AWS_TS_CDK_Udemy_Course/space-finder/tests/auth.test.ts
+++ AWS_TS_CDK_Udemy_Course/space-finder/tests/auth.test.ts
@@ -1,3 +1,4 @@
+import { ListBucketsCommand, S3Client } from '@aws-sdk/client-s3';
 import { AuthService } from './AuthService';
 
 async function testAuth() {
@@ -5,7 +6,18 @@ async function testAuth() {
   const loginRequest = await service.login('vvstor', 'J-LzxT6ngpptcus');
   // console.log(loginRequest.getSignInUserSession().getIdToken().getJwtToken());
   const credentials = await service.generateTemporaryCredentials(loginRequest);
-  console.log(credentials);
+  // console.log(credentials);
+  const buckets = await listBuckets(credentials);
+  console.log(buckets);
+}
+
+async function listBuckets(credentials: any) {
+  const client = new S3Client({
+    credentials: credentials,
+  });
+  const command = new ListBucketsCommand({});
+  const result = await client.send(command);
+  return result;
 }
 
 testAuth();
