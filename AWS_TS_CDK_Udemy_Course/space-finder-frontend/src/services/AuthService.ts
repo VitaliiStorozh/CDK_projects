b427073229c47859c1039012e057c978719c4293
Vitalii Storozh <storozh.softserve@gmail.com>
2023-07-19 22:29:47 +0300

    Login component

---
+++ AWS_TS_CDK_Udemy_Course/space-finder-frontend/src/services/AuthService.ts
@@ -0,0 +1,17 @@
+
+
+
+
+export class AuthService {
+
+
+    public async login(userName: string, password: string): Promise<Object | undefined> {
+        return {
+            user: 'abc'
+        }
+    }
+
+    public getUserName(){
+        return 'some user'
+    }
+}
