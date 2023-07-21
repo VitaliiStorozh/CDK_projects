7a4e2b7c2a732f7a336b5d8ebb8aa9a78fce58f1
Vitalii Storozh <storozh.softserve@gmail.com>
2023-07-21 16:27:46 +0300

    Creating spaces

---
+++ AWS_TS_CDK_Udemy_Course/space-finder-frontend/src/services/DataService.ts
@@ -0,0 +1,14 @@
+
+
+
+export class DataService {
+
+
+    public async createSpace(name: string, location: string, photo?: File){
+        return 'space created'
+    }
+
+    public isAuthorized(){
+        return true
+    }
+}
