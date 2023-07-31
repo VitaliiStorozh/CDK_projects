27644a79047cc7d329de969e6edaa0636609d92e
Vitalii Storozh <storozh.softserve@gmail.com>
2023-07-31 14:12:22 +0300

    CDK alarm

--- AWS_TS_CDK_Udemy_Course/space-finder/src/services/monitor/handler.ts
+++ AWS_TS_CDK_Udemy_Course/space-finder/src/services/monitor/handler.ts
@@ -1,7 +1,7 @@
 import { SNSEvent } from 'aws-lambda';
 
 const webHookUrl =
-  'https://hooks.slack.com/services/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';
+  'https://hooks.slack.com/services/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';
 
 async function handler(event: SNSEvent, context) {
   for (const record of event.Records) {
