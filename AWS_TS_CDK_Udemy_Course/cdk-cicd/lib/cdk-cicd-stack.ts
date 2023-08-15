885eb5d4962e6f7df395a4dd27388103d319ac43
Vitalii Storozh <storozh.softserve@gmail.com>
2023-08-15 17:46:59 +0300

    Init Pipeline

--- AWS_TS_CDK_Udemy_Course/cdk-cicd/lib/cdk-cicd-stack.ts
+++ AWS_TS_CDK_Udemy_Course/cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyServicePipeline',
+        synth: new ShellStep('Synth', {
+            input: CodePipelineSource.gitHub('VitaliiStorozh/CDK_projects', 'main'),
+            commands: [
+                'cd AWS_TS_CDK_Udemy_Course/cdk-cicd',
+                'npm ci',
+                'npx cdk synth'
+            ],
+          primaryOutputDirectory: 'AWS_TS_CDK_Udemy_Course/cdk-cicd/cdk.out',
+        }),
+    })
   }
 }
