f994a340a6ada703353fdcc45664ad203af2fb3e
Vitalii Storozh <storozh.softserve@gmail.com>
2023-08-16 20:51:01 +0300

    Added hello lambda and esbuild

--- AWS_TS_CDK_Udemy_Course/cdk-cicd/lib/LambdaStack.ts
+++ AWS_TS_CDK_Udemy_Course/cdk-cicd/lib/LambdaStack.ts
@@ -1,5 +1,8 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
 
 
 interface LambdaStackProps extends StackProps {
@@ -9,5 +12,14 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
     constructor(scope: Construct, id: string, props?: LambdaStackProps) {
         super(scope, id, props);
+
+        new NodejsFunction(this, 'Lambda', {
+            runtime: Runtime.NODEJS_18_X,
+            entry: (join(__dirname, '..', 'services', 'hello.ts')),
+            handler: 'handler',
+            environment: {
+                STAGE: props?.stageName || 'dev'
+            }
+        })
     }
 }
